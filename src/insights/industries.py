8ced78eb6728bbc01704909e0cda271a111d3326
Isabela Rozani Floriano <irf161295@gmail.com>
2022-12-12 16:16:36 -0300

    feat: create function to get a list of unique values ​​present in the industry column

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,13 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    jobs_list = read(path)
+    unique_industries = set(
+        job["industry"]
+        for job in jobs_list
+        if job["industry"] != ""
+        )
+    return unique_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
