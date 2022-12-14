f9f713ac1057e766b22d19eed7eeb2fc63cd215e
Isabela Rozani Floriano <irf161295@gmail.com>
2022-12-14 20:55:27 -0300

    feat: create test for brazilian_jobs function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result_list = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    result_dict_keys = [
+        result.keys()
+        for result in result_list
+        ]
+    for dict in result_dict_keys:
+        # print(dict == "dict_keys(['title', 'salary', 'type'])")
+        assert str(dict) == "dict_keys(['title', 'salary', 'type'])"
